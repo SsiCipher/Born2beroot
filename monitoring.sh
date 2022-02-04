8b1204d50652740e5265310c2758e0a1445fb5c9
SsiCipher <94566747+SsiCipher@users.noreply.github.com>
2022-02-04 01:18:05 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,31 +1,54 @@
 #!/bin/sh
 
+# Architecture
 arch=$(uname -a)
-cpu=$(cat /proc/cpuinfo | grep -c processor)
-vcpu=$(cat /proc/cpuinfo | grep -c physical\ id)
+
+# CPU
+cpu=$(nproc)
+vcpu=$(grep -c processor /proc/cpuinfo)
+
+# RAM
 ram_used=$(free --mega | grep Mem | xargs | cut -d " " -f 3)
 ram_total=$(free --mega | grep Mem | xargs | cut -d " " -f 2)
+ram_usage=$(free --mega | awk 'NR==2 {printf("%.2f%%", $3*100/$2)}')
+
+#Disk
 disk_used=$(df -H --total | grep total | xargs | cut -d " " -f 3 | tr -d "[A-Za-z]")
 disk_total=$(df -H --total | grep total | xargs | cut -d " " -f 2 | tr -d "[A-Za-z]")
 disk_percentage=$(df -H --total | grep total | xargs | cut -d " " -f 5)
-cpu_load=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-last_boot_date=$(who | xargs | cut -d " " -f 3)
-last_boot_time=$(who | xargs | cut -d " " -f 4)
+
+# CPU Load
+cpu_load=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3 + $5}')
+
+# Last Boot
+last_boot=$(uptime -s | awk '{printf("%s %s", $1, substr($2, 0, 5))}')
+
+# LVM
 lvm_active=$(cat /etc/fstab | grep -c /dev/mapper)
-tcp_conn=$(cat /proc/net/sockstat | grep TCP | cut -d " " -f 3)
+
+# TCP Conns
+tcp_conn=$(ss -t | grep -c ESTAB)
+
+# USERS
 logged_users=$(who | wc -l)
+
+# IP
 ip_address=$(hostname -I | xargs)
+
+# MAC
 mac_address=$(ip link | grep link/ether | xargs | cut -d " " -f 2)
-sudo_cmds=$(cat /var/log/auth.log | grep -c sudo)
+
+# SUDO
+sudo_cmds=$(grep -c COMMAND /var/log/sudo/sudo.log)
 
 wall "
 	#Architecture: $arch
 	#CPU physical: $cpu
 	#vCPU: $vcpu
-	#Memory Usage: $ram_used/${ram_total}MB ($(($ram_used * 100 / $ram_total))%)
+	#Memory Usage: $ram_used/${ram_total}MB ($ram_usage)
 	#Disk Usage: $disk_used/${disk_total}Gb ($disk_percentage)
 	#CPU load: $cpu_load
-	#Last boot: $last_boot_date $last_boot_time
+	#Last boot: $last_boot
 	#LVM use: $(if [ $lvm_active -gt 0 ]; then echo "yes"; else echo "no"; fi)
 	#Connexions TCP: $tcp_conn ESTABLISHED
 	#User log: $logged_users
